6f0042518e02ce20ca52030d5fe84d5fd0d43ed2
Cayden France <cfcodinginspire@gmail.com>
2025-08-30 16:15:32 -0500

    updated product listing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import './src/main/styles/home.css';
-
-// Import your existing components
-import HomePage from './components/HomePage';
-import CandlesPage from './components/CandlesPage';
-import DiffusersPage from './components/DiffusersPage';
-import WaxMeltsPage from './components/WaxMeltsPage';
-import CheckoutPage from './components/CheckoutPage';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/pages/candles" element={<CandlesPage />} />
-          <Route path="/pages/diffusers" element={<DiffusersPage />} />
-          <Route path="/pages/wax-melts" element={<WaxMeltsPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
